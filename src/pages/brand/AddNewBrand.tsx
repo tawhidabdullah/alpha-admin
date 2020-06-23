849839ab28ec7fe5ee728939022ee90621a4fc09
tawhidabdullah <tawhidabdullah22@gmail.com>
2020-06-24 03:41:50 +0600

    styled/added image library

--- src/pages/brand/AddNewBrand.tsx
+++ src/pages/brand/AddNewBrand.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // import third party ui lib
-import { Upload, message, Switch, Select, Button, notification } from 'antd';
+import { Upload, message, Switch, Select, Button, notification, Modal, Tabs} from 'antd';
 
 import {
 	FileOutlined,
@@ -9,7 +9,10 @@ import {
 	RadiusUpleftOutlined,
 	RadiusUprightOutlined,
 	RadiusBottomleftOutlined,
-	RadiusBottomrightOutlined
+	RadiusBottomrightOutlined,
+	CheckOutlined,
+	FileAddOutlined, 
+	ArrowUpOutlined
 } from '@ant-design/icons';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -19,6 +22,7 @@ import Input from '../../components/Field/Input';
 
 const { Dragger } = Upload;
 const { Option } = Select;
+const { TabPane } = Tabs;
 
 const props = {
 	name: 'file',
@@ -44,7 +48,24 @@ const AddNewBrand = ({  }: Props) => {
 		console.log(checked);
 	};
 
+	const [visible,setvisible] = useState(false);   
+
+
+
+    const handleOk = (e: any) => {
+        setvisible(false);
+      
+      };
+    
+      const handleCancel = (e: any) => {
+        setvisible(false);
+      };
+
+
+
+
 	return (
+		<>
 		<div className='site-layout-background' style={{ padding: '30px 50px 30px 50px', minHeight: 360 }}>
 			<div className='addproductSectionTitleContainer'>
 				<h2 className='addprouctSectionTitle'>Add Brand</h2>
@@ -61,7 +82,7 @@ const AddNewBrand = ({  }: Props) => {
 					/>
 					<div className='addproductSection-left-header'>
 						<h3 className='inputFieldLabel'>Images</h3>
-						<div>
+						<div onClick={()=> setvisible(true)}>
 							<FileOutlined />
 							<span>Media Center</span>
 						</div>
@@ -95,6 +116,142 @@ const AddNewBrand = ({  }: Props) => {
 				</div>
 			</div>
 		</div>
+		<Modal
+          title="Media Library"
+          visible={visible}
+          onOk={handleOk}
+		  onCancel={handleCancel}
+		  width={'80vw'}
+		  bodyStyle={{
+			  margin: '0',
+			  padding: '0'
+		  }}
+		  okText='Done'
+        >
+          <div className='mediaLibraryBodyContainer'>
+						<div className='mediaLibraryBodyContainer-left'>
+
+		<Tabs defaultActiveKey="2" type="card" size='middle'>
+          <TabPane tab="Upload New Media" key="1">
+		  <div className='mediaLibraryBodyContainer-left-header'>
+						
+					<div>
+						<Dragger
+							style={{
+								background: '#fff'
+							}}
+							{...props}
+						>
+							<p className='ant-upload-drag-icon'>
+								<InboxOutlined />
+							</p>
+							<p className='ant-upload-text'>Click or drag file to this area to upload</p>
+							<p className='ant-upload-hint'>
+								Support for a single or bulk upload. Strictly prohibit from uploading company data or
+								other band files
+							</p>
+						</Dragger>
+					</div>
+							</div>
+
+							<div style={{
+								marginTop: '20px'
+							}}></div>
+
+							<Button 
+							type='primary'
+							icon={<ArrowUpOutlined />}
+							 onClick={() => console.log('createCategory')}>
+						Upload
+					</Button>
+
+          </TabPane>
+          <TabPane tab="Media Library" key="2">
+		  <div className='mediaLibraryBodyContainer-left-imageListContainer'>
+		  							<div  className='mediaLibraryBodyContainer-left-imageListContainer-item'>
+										  <div className='mediaLibraryBodyContainer-left-imageListContainer-item-icon'>
+											  <CheckOutlined 
+											  
+											  />
+										  </div>
+									  <img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+		  								
+									  </div>
+									  <div  className='mediaLibraryBodyContainer-left-imageListContainer-item'>
+									  <img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+		  								
+									  </div>
+									  <div  className='mediaLibraryBodyContainer-left-imageListContainer-item'>
+									  <img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+		  								
+									  </div>
+									  <div  className='mediaLibraryBodyContainer-left-imageListContainer-item'>
+									  <img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+		  								
+									  </div>
+									  <div  className='mediaLibraryBodyContainer-left-imageListContainer-item'>
+									  <img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+		  								
+									  </div>
+									  <div  className='mediaLibraryBodyContainer-left-imageListContainer-item'>
+									  <img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+		  								
+									  </div>
+									  <div  className='mediaLibraryBodyContainer-left-imageListContainer-item'>
+									  <img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+		  								
+									  </div>
+
+							  </div>
+          </TabPane>
+        
+         </Tabs>
+
+
+		  				
+						</div>
+						<div className='mediaLibraryBodyContainer-right'>
+							<h4>
+								Attachment Details
+							</h4>
+							<div className='mediaLibraryBodyContainer-right-ImageDetails'>
+								<div className='mediaLibraryBodyContainer-right-ImageDetails-imageContainer'>
+									<img src='https://homebazarshibchar.com/images/library/thumbnail/600043-cartScreen.jpg' alt='img' />
+								</div>
+								<div className='mediaLibraryBodyContainer-right-ImageDetails-infoContainer'>
+									<h5 className='imageLibnameText'>
+										IMG_1104.jpg
+									</h5>
+									<h5>
+										April 20,1204
+									</h5>
+									<h5>
+										5000 X 500
+									</h5>
+									<h5 className='imageLibdeleteText'>
+										Delete parmanently
+									</h5>
+								</div>
+							</div>
+							<div>
+							<Input label='Title' />
+							<Input label='Alternate Text' />
+							<Input label='Caption' />
+							<Input label='Label' />
+							<div
+						style={{
+							marginTop: '20px'
+						}}
+					/>
+					<Button type='primary' onClick={() => console.log('createCategory')}>
+						Update
+					</Button>
+							</div>
+						</div>
+		  </div>
+		  
+        </Modal>
+		</>
 	);
 };
 
