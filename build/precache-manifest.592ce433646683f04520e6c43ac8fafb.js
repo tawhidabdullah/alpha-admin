5b6472dcc7196880648c24cd6a446e386b248dce
tawhidabdullah <tawhidabdullah22@gmail.com>
2020-09-14 19:34:12 +0600

    builded and pushed sheratong

---
+++ build/precache-manifest.592ce433646683f04520e6c43ac8fafb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4aa6b398fd8847dc0b5c9a26525ee3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d9d9cd81e29d8178f26",
+    "url": "/static/css/2.7c6d6ba6.chunk.css"
+  },
+  {
+    "revision": "5e192eb5aeae1daed206",
+    "url": "/static/css/main.ba301e3c.chunk.css"
+  },
+  {
+    "revision": "2d9d9cd81e29d8178f26",
+    "url": "/static/js/2.1ea07b4f.chunk.js"
+  },
+  {
+    "revision": "570e3ca0634b367e39dc1015f9827f50",
+    "url": "/static/js/2.1ea07b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e192eb5aeae1daed206",
+    "url": "/static/js/main.29eb137a.chunk.js"
+  },
+  {
+    "revision": "75438d04c1eee39a8689",
+    "url": "/static/js/runtime-main.803277ad.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b378070c28c885132e2e162b67e9392.js"
+  "/precache-manifest.592ce433646683f04520e6c43ac8fafb.js"
 );
 
 self.addEventListener('message', (event) => {
