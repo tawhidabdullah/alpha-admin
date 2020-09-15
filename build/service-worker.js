528c73575f7e4c4de1e78331d055ff2bee20d764
tawhidabdullah <tawhidabdullah22@gmail.com>
2020-09-15 17:37:49 +0600

    builded zirabo

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dd446a11f500ca28ffa25a119fd64e4.js"
+  "/precache-manifest.9755254e603c42c2c6a99d2aa88d8eb9.js"
 );
 
 self.addEventListener('message', (event) => {
