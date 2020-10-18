ca95f3724fc9a6d2eebfb9fd6caf064717ebd3b8
tawhidabdullah <tawhidabdullah22@gmail.com>
2020-10-18 16:26:14 +0600

    builded

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.849a31e98bedac8f87e9518985369aa6.js"
+  "/precache-manifest.2b5f5bbb32c90c4f249e4db4df327c95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
