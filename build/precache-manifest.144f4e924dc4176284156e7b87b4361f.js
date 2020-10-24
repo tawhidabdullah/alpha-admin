16751cb30271a8dc98222a838dce0c73d7ea65d8
tawhidabdullah <tawhidabdullah22@gmail.com>
2020-10-24 12:41:09 +0600

    builed sharaexpress

---
+++ build/precache-manifest.144f4e924dc4176284156e7b87b4361f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b413315018fcdfe8963d83b0c922cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf062523f61ddc63c53b",
+    "url": "/static/css/2.c4faeff2.chunk.css"
+  },
+  {
+    "revision": "a403b77657913d108422",
+    "url": "/static/css/main.f1788708.chunk.css"
+  },
+  {
+    "revision": "cf062523f61ddc63c53b",
+    "url": "/static/js/2.701ad52f.chunk.js"
+  },
+  {
+    "revision": "65826839b9c06da18c438384ff42acbb",
+    "url": "/static/js/2.701ad52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a403b77657913d108422",
+    "url": "/static/js/main.9ab18a6c.chunk.js"
+  },
+  {
+    "revision": "75438d04c1eee39a8689",
+    "url": "/static/js/runtime-main.803277ad.js"
+  }
+]);
